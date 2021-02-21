5fb3759867a8cb68c6863b057733da8924f5ed8f
john <borges-2014@live.com>
2021-02-21 11:49:04 -0300

    Add prev and next icons in react

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import { Carousel } from 'react-bootstrap';
 
 function App() {
   const slides = Array.from(
-    document.querySelectorAll('#slides .slide')
+    document.querySelectorAll('#slides .slide'),
   ).map((slide) => slide.innerHTML);
   return (
     <div className="App">
       <Carousel
         interval={null}
-        nextIcon={<span aria-hidden="true" className="carousel-control-next-icon" />}
+        indicators={false}
+        prevIcon={<img
+                    src="/css/chevron-left.svg"
+                    alt="prev slide"
+                    className="position-fixed"
+                    width="50"
+                    height="75"
+                  />}
+        nextIcon={<img
+                      src="/css/chevron-right.svg"
+                      alt="next slide"
+                      className="position-fixed"
+                      width="50"
+                      height="75"
+                  />}
       >
         {slides.map((slide, id) => (
           <Carousel.Item
