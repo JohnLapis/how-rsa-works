d5550bffd687412570ac2b0912c6cc305a9689a4
john <borges-2014@live.com>
2021-02-24 08:51:15 -0300

    chore: Remove unused code

--- src/components/NextButton.js
+++
@@ -1,6 +0,0 @@
-export default function NextButton(props) {
-  section = get_somehow_current_section()
-  nextSectionId = "section-" + (+section.match(/section-(\d+)/)[1] + 1)
-  nextSection = docs.querySelector(`#${nextSectionId}`)
-  if (nextSection) transition(section, nextSection)
-}
